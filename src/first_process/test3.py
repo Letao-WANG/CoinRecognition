a916741154d07f48c086121024d9b448d1ef8eed
Letao <letaowang7@gmail.com>
2022-03-16 10:11:20 +0100

    Syu

--- src/first_process/test3.py
+++ src/first_process/test3.py
@@ -4,39 +4,39 @@ import json
 
 ratio = 0.5
 
+
 # Retourne l'ensemble des rectangles qui ne sont pas inclut dans un autre
 def checkSquareIn(t1):
-
     t2 = []
     # Pour chaque éléments de t1
-    for c in range(len(t1)) :
+    for c in range(len(t1)):
         booleen = True
         # On récupére les coordonnées 
-        y1 =t1[c][1]
+        y1 = t1[c][1]
         x1 = t1[c][0]
-        y1f =t1[c][3]
-        x1f =t1[c][2]
-        #Pour chaque éléments de t1
-        for d in range(0,len(t1)) :
+        y1f = t1[c][3]
+        x1f = t1[c][2]
+        # Pour chaque éléments de t1
+        for d in range(0, len(t1)):
             # On récupére les coordonnées 
             x2 = t1[d][0]
-            y2 =t1[d][1]     
-            x2f =t1[d][2]       
+            y2 = t1[d][1]
+            x2f = t1[d][2]
             y2f = t1[d][3]
             # Vérification d'inclusion 
-            if x1>x2 and x1<x2f: # x du premier > au xdu second
-                if x1f > x2 and x1f<x2f: # x final du premier < au x final du second
-                    if y1>y2 and y1<y2f:
-                        if y1f>y2 and y1f<y2f:
+            if x1 > x2 and x1 < x2f:  # x du premier > au xdu second
+                if x1f > x2 and x1f < x2f:  # x final du premier < au x final du second
+                    if y1 > y2 and y1 < y2f:
+                        if y1f > y2 and y1f < y2f:
                             booleen = False
-                            
 
-        if booleen :
+        if booleen:
             t2.append(t1[c])
-        else :
+        else:
             if t1[c] in t2:
                 t2.remove(t1[c])
     return t2
+
 
 # Vérifie si deux rectangles ont une intersection, si plus de 50%
 # est partagé, les réunis, sinon ne fais rien 
@@ -44,112 +44,108 @@ def SquareIntersection(t1):
     t2 = []
     t4 = []
     tcopy = []
-    for l in range(0,len(t1)) :
+    for l in range(0, len(t1)):
         tcopy.append(t1[l])
 
     doisRefaire = True
 
-    while(doisRefaire):
+    while (doisRefaire):
         doisRefaire = False
         t2 = []
-        for c in range(0,len(tcopy)) :
+        for c in range(0, len(tcopy)):
             if doisRefaire:
                 break
             booleen = False
-            y1 =tcopy[c][1]
+            y1 = tcopy[c][1]
             x1 = tcopy[c][0]
-            y1f =tcopy[c][3]
-            x1f =tcopy[c][2]
+            y1f = tcopy[c][3]
+            x1f = tcopy[c][2]
             estModifie = False
             lenn = len(tcopy)
-            for d in range(0,lenn) :
+            for d in range(0, lenn):
                 x2 = tcopy[d][0]
-                y2 =tcopy[d][1]     
-                x2f =tcopy[d][2]       
+                y2 = tcopy[d][1]
+                x2f = tcopy[d][2]
                 y2f = tcopy[d][3]
-                
-                
-                
+
                 booleen = False
-                #En haut à gauche
-                if x2>x1 and x2<x1f and y2>y1  and y2<y1f:
+                # En haut à gauche
+                if x2 > x1 and x2 < x1f and y2 > y1 and y2 < y1f:
                     booleen = True
-                    if x2f>x1f:
+                    if x2f > x1f:
                         longueur = x1f - x2
                     else:
-                        longueur = x2f -x2     
+                        longueur = x2f - x2
 
-                    if y2f>y1:
-                        hauteur = y1f-y2
+                    if y2f > y1:
+                        hauteur = y1f - y2
                     else:
-                        hauteur = y2f-y2
-                #En haut à droite
-                elif x2f>x1 and x2f<x1f and y2>y1  and y2<y1f:
+                        hauteur = y2f - y2
+                # En haut à droite
+                elif x2f > x1 and x2f < x1f and y2 > y1 and y2 < y1f:
                     booleen = True
-                    if x2<x1:
+                    if x2 < x1:
                         longueur = x2f - x1
                     else:
-                        longueur = x2f -x2     
+                        longueur = x2f - x2
 
-                    if y2f>y1:
-                        hauteur = y1f-y2
+                    if y2f > y1:
+                        hauteur = y1f - y2
                     else:
-                        hauteur = y2f-y2
-                #En bas à gauche
-                elif x2>x1 and x2<x1f and y2f>y1  and y2f<y1f:
+                        hauteur = y2f - y2
+                # En bas à gauche
+                elif x2 > x1 and x2 < x1f and y2f > y1 and y2f < y1f:
                     booleen = True
-                    if x2f>x1f:
+                    if x2f > x1f:
                         longueur = x1f - x2
                     else:
-                        longueur = x2f -x2     
+                        longueur = x2f - x2
 
-                    if y2<y1:
-                        hauteur = y2f-y1
+                    if y2 < y1:
+                        hauteur = y2f - y1
                     else:
-                        hauteur = y2f-y2
-                #En bas a droite
-                elif x2f>x1 and x2f<x1f and y2f>y1  and y2f<y1f:
+                        hauteur = y2f - y2
+                # En bas a droite
+                elif x2f > x1 and x2f < x1f and y2f > y1 and y2f < y1f:
                     booleen = True
-                    if x2<x1:
+                    if x2 < x1:
                         longueur = x2f - x1
                     else:
-                        longueur = x2f -x2    
+                        longueur = x2f - x2
 
-                    if y2<y1:
-                        hauteur = y2f-y1
+                    if y2 < y1:
+                        hauteur = y2f - y1
                     else:
-                        hauteur = y2f-y2                        
+                        hauteur = y2f - y2
                 if booleen:
                     aire = hauteur * longueur
-                    aire1 = (x1f-x1)*(y1f-y1)
-                    aire2 = (x2f-x2)*(y2f-y2)
-                    ratio1 = aire/aire1
-                    ratio2 = aire/aire2
-                    if ratio1>=0.4 or ratio2 >= 0.4:
-                        newSquare = CombineSquare(tcopy[c],tcopy[d])
+                    aire1 = (x1f - x1) * (y1f - y1)
+                    aire2 = (x2f - x2) * (y2f - y2)
+                    ratio1 = aire / aire1
+                    ratio2 = aire / aire2
+                    if ratio1 >= 0.4 or ratio2 >= 0.4:
+                        newSquare = CombineSquare(tcopy[c], tcopy[d])
                         estModifie = True
                         tcopy.pop(d)
-                        if(d<c):
-                            ind = c-1
+                        if (d < c):
+                            ind = c - 1
                         else:
                             ind = c
                         tcopy.pop(ind)
                         tcopy.append(newSquare)
-                        c = lenn+1
-                        d = lenn+1
+                        c = lenn + 1
+                        d = lenn + 1
                         doisRefaire = True
                         break
 
-        
-            if not(estModifie):
+            if not (estModifie):
                 t2.append(tcopy[c])
 
     return t2
 
 
-    
-#Combine deux carrés, retourne un tuple de la forme (x,y,xf,yf).
-def CombineSquare(square1,square2):
+# Combine deux carrés, retourne un tuple de la forme (x,y,xf,yf).
+def CombineSquare(square1, square2):
     x1 = square1[0]
     x1f = square1[2]
     y1 = square1[1]
@@ -160,57 +156,57 @@ def CombineSquare(square1,square2):
     y2 = square2[1]
     y2f = square2[3]
 
-    if x1<x2 :
+    if x1 < x2:
         x = x1
     else:
         x = x2
-    if x1f>x2f :
+    if x1f > x2f:
         xf = x1f
     else:
         xf = x2f
-    if y1<y2 :
+    if y1 < y2:
         y = y1
     else:
         y = y2
-    if y1f>y2f :
+    if y1f > y2f:
         yf = y1f
     else:
         yf = y2f
 
-    return (x,y,xf,yf)
-    
-    
-#Affiche les rectangles        
-def getContours(img, imgContour,imgNormal):
+    return (x, y, xf, yf)
+
+
+# Affiche les rectangles
+def getContours(img, imgContour, imgNormal):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #cv2.drawContours(imgContour, contours, -1, (255, 0, 255), 7)
+    # cv2.drawContours(imgContour, contours, -1, (255, 0, 255), 7)
     t = []
     ind = 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 8000: #7000
+        if area > 8000:  # 7000
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            #print(str(len(approx)) + " " + str(area))
+            # print(str(len(approx)) + " " + str(area))
             x, y, w, h = cv2.boundingRect(approx)
 
             plusGrand = w
             plusPetit = h
-            if h> w:
+            if h > w:
                 plusGrand = h
                 plusPetit = w
-            if plusPetit/plusGrand >ratio :
-                t.append((x,y,x + w, y + h))
-            ind+=1
+            if plusPetit / plusGrand > ratio:
+                t.append((x, y, x + w, y + h))
+            ind += 1
 
-    #t3 = checkSquareIn(t)
+    # t3 = checkSquareIn(t)
     t3 = SquareIntersection(t)
     images = []
     for c in range(len(t3)):
-        cv2.rectangle(imgContour, (t3[c][0],t3[c][1]),(t3[c][2],t3[c][3]) , (0, 255, 0), 5)
-        images.append(imgNormal[t3[c][1]:t3[c][3],t3[c][0]:t3[c][2]] )
-        #cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.rectangle(imgContour, (t3[c][0], t3[c][1]), (t3[c][2], t3[c][3]), (0, 255, 0), 5)
+        images.append(imgNormal[t3[c][1]:t3[c][3], t3[c][0]:t3[c][2]])
+        # cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
     return images
 
 
@@ -230,13 +226,13 @@ def main():
     sommeSuccess = 0
 
     # Pour chaque image
-    for a in range(0, 60,2):
-        threshold1 = 50 #20
-        threshold2 = 60 #80
+    for a in range(0, 60, 2):
+        threshold1 = 50  # 20
+        threshold2 = 60  # 80
         kernel = np.ones((5, 5), np.float32) / 25
 
         img = check_empty_img(a)
-        if img is None :
+        if img is None:
             continue
         imgContour = img.copy()
 
@@ -245,41 +241,41 @@ def main():
         imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
         imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
-        images = getContours(imgDil, imgContour,img)
-        if(len(images) == 0):     
+        images = getContours(imgDil, imgContour, img)
+        if (len(images) == 0):
             imgBlur = cv2.GaussianBlur(img, (7, 7), 5)
-            #imgBlur = cv2.GaussianBlur(img, (21, 21), cv2.BORDER_DEFAULT)
+            # imgBlur = cv2.GaussianBlur(img, (21, 21), cv2.BORDER_DEFAULT)
             imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
             imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
             imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
-            images = getContours(imgDil, imgContour,img)
+            images = getContours(imgDil, imgContour, img)
 
         with open(r"D:\COur\img_proj\\" + str(a) + ".json") as json_data:
             data_dict = json.load(json_data)
-            if(len(data_dict["shapes"])==len(images)):
-                print("image "+str(a)+" : Success")
+            if (len(data_dict["shapes"]) == len(images)):
+                print("image " + str(a) + " : Success")
                 sommeSuccess += 1
             else:
-                print("image "+str(a)+" : Failure")
+                print("image " + str(a) + " : Failure")
 
         show = False
-        if(show):
+        if (show):
             for i in range(len(images)):
-                cv2.imshow("image",images[i])
-                cv2.waitKey(0) 
-                cv2.destroyAllWindows() 
+                cv2.imshow("image", images[i])
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
-
-        # cv2.imshow("1 normal",img)
+                # cv2.imshow("1 normal",img)
         # cv2.imshow("2 Blur",imgBlur)
         # cv2.imshow("3 GrayScale",imgGray)
         # cv2.imshow("4 Canny",imgCanny)
         # cv2.imshow("5 Dilatation",imgDil)
         # cv2.imshow("6 Contour",imgContour)
 
-        cv2.imwrite(str(a)+".jpg", imgContour)
+        cv2.imwrite(str(a) + ".jpg", imgContour)
 
     print(str(sommeSuccess) + " Success")
+
 
 main()
--- src/second_process/projet_test.py
+++ src/second_process/projet_test.py
@@ -1,154 +1,110 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-from skimage.transform import resize
+import math
+import cv2
 
+from util_func.util import *
 
-def get_image_rgb(origin_image):
-    size = origin_image.shape
-    res = np.zeros([size[0], size[1], 3], dtype=np.uint8)
-    for i in range(size[0]):
-        for j in range(size[1]):
-            res[i, j, 0] = origin_image[i, j, 0]
-            res[i, j, 1] = origin_image[i, j, 1]
-            res[i, j, 2] = origin_image[i, j, 2]
-    return res
-
-
-def get_histogram(img):
-    histogram = [0] * 255
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            histogram[int(img[i, j])] += 1
-    return histogram
-
-
-def get_histogram_cumule(img):
-    histogram = get_histogram(img)
-    histogram_cumule = [0] * 255
-    histogram_cumule[0] = histogram[0]
-    for i in range(1, 255):
-        histogram_cumule[i] = histogram[i] + histogram_cumule[i - 1]
-    return histogram_cumule
-
-
-def divide_image_rpg(image_rgb):
-    size = image_rgb.shape
-    image_r = np.zeros([size[0], size[1]], dtype=np.uint8)
-    image_g = np.zeros([size[0], size[1]], dtype=np.uint8)
-    image_b = np.zeros([size[0], size[1]], dtype=np.uint8)
-    for i in range(size[0]):
-        for j in range(size[1]):
-            image_r[i, j] = image_rgb[i, j, 0]
-            image_g[i, j] = image_rgb[i, j, 1]
-            image_b[i, j] = image_rgb[i, j, 2]
-    return image_r, image_g, image_b
-
-
-def equalization(image):
-    image_egalization = np.zeros(image.shape, dtype=np.uint8)
-    histogram_cumule = get_histogram_cumule(image)
-    n = 255
-    N = image.shape[0] * image.shape[1]
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            c = histogram_cumule[image[i, j]]
-            image_egalization[i, j] = max(0, (n / N) * c - 1)
-    return image_egalization
-
-
-def equalization_rgb(image_rgb):
-    image_r, image_b, image_g = divide_image_rpg(image_rgb)
-    image_r_eq = equalization(image_r)
-    image_g_eq = equalization(image_g)
-    image_b_eq = equalization(image_b)
-    return fusion_image_rgb(image_r_eq, image_g_eq, image_b_eq)
-
-
-def fusion_image_rgb(image_r, image_g, image_b):
-    size = image_r.shape
-    image_rgb = np.zeros([size[0], size[1], 3], dtype=np.uint8)
-    for i in range(size[0]):
-        for j in range(size[1]):
-            image_rgb[i, j, 0] = image_r[i, j]
-            image_rgb[i, j, 1] = image_g[i, j]
-            image_rgb[i, j, 2] = image_b[i, j]
-    return image_rgb
-
-
-# amplitude = 1
-# histogram = [0] * 255
-# for i in range(img.shape[0]):
-#     for j in range(img.shape[1]):
-#         histogram[int(img[i, j])] += 1
-#
-# print(histogram)
-#
-# histogram_cumule = [0] * 255
-# histogram_cumule[0] = histogram[0]
-# for i in range(1, 255):
-#     histogram_cumule[i] = histogram[i] + histogram_cumule[i - 1]
-#
-# print(histogram_cumule)
-
-# Egalisation
-
-# img_egalisation = np.zeros(img.shape, dtype=np.uint8)
-# n = 255
-# # N = histogram_cumule[len(histogram_cumule)-1]
-# N = img.shape[0] * img.shape[1]
-# for i in range(img.shape[0]):
-#     for j in range(img.shape[1]):
-#         c = histogram_cumule[img[i, j]]
-#         img_egalisation[i, j] = max(0, (n/N) * c - 1)
-
-
-# def otsu(gray):
-#     pixel_number = gray.shape[0] * gray.shape[1]
-#     mean_weight = 1.0/pixel_number
-#     his, bins = np.histogram(gray, np.arange(0, 257))
-#     final_thresh = -1
-#     final_value = -1
-#     intensity_arr = np.arange(256)
-#     for t in bins[1:-1]: # This goes from 1 to 254 uint8 range (Pretty sure wont be those values)
-#         pcb = np.sum(his[:t])
-#         pcf = np.sum(his[t:])
-#         Wb = pcb * mean_weight
-#         Wf = pcf * mean_weight
-#
-#         mub = np.sum(intensity_arr[:t]*his[:t]) / float(pcb)
-#         muf = np.sum(intensity_arr[t:]*his[t:]) / float(pcf)
-#         #print mub, muf
-#         value = Wb * Wf * (mub - muf) ** 2
-#
-#         if value > final_value:
-#             final_thresh = t
-#             final_value = value
-#     final_img = gray.copy()
-#     final_img[gray > final_thresh] = 255
-#     final_img[gray < final_thresh] = 0
-#     return final_img
 
 
 # plt.imshow(img, cmap=plt.cm.gray, vmin=0, vmax=max_val)
 # plt.show()
 
 
+def filter_sobel_x():
+    return np.array([[-1, 0, 1],
+                     [-2, 0, 2],
+                     [-1, 0, 1]])
+
+
+def filter_sobel_y():
+    return np.array([[1, 2, 1],
+                     [0, 0, 0],
+                     [-1, -2, -1]])
+
+
+def expend_image_size(origin_img):
+    new_image = np.zeros(shape=(origin_img.shape[0]+2, origin_img.shape[1]+2))
+    new_image[1:origin_img.shape[0]+1, 1:origin_img.shape[1]+1] = origin_img
+    return new_image
+
+
+def convolution_sobel(gray_img):
+    gray_img = expend_image_size(gray_img)
+    row = gray_img.shape[0]
+    col = gray_img.shape[1]
+
+    # result = np.zeros(shape=(row, col)).astype(np.uint8)
+    result = np.zeros(shape=(row, col))
+    for i in range(row-2):
+        for j in range(col-2):
+            current = gray_img[i:i+3, j:j+3]
+            multiplication_x = np.abs(sum(sum(current * filter_sobel_x())))
+            multiplication_y = np.abs(sum(sum(current * filter_sobel_y())))
+            result[i, j] = math.sqrt(multiplication_x**2 + multiplication_y**2)
+
+            # result[i, j] = multiplication_x
+    return result
+
+
+def convolution_filter(gray_img, kernel):
+    kernel_size = len(kernel)
+    gray_img = expend_image_size(gray_img)
+    row = gray_img.shape[0]
+    col = gray_img.shape[1]
+
+    result = np.zeros(shape=(row, col))
+
+    for i in range(row-2):
+        for j in range(col-2):
+            current = gray_img[i:i+kernel_size, j:j+kernel_size]
+            multiplication = np.abs(sum(sum(current * kernel)))
+            result[i, j] = multiplication
+    return result
+
+
 def main():
     image = (mpimg.imread('test_images/1_1.png').copy() * 255).astype(np.uint8)
-    image_rgb = get_image_rgb(image)
+    # image = mpimg.imread('test_images/Bikesgray.jpg')
+    # image = (mpimg.imread('test_images/1_2.png').copy() * 255).astype(np.uint8)
+    image = get_image_rgb(image)
+    image = get_image_gray(image)
     # image_eq = equalization_rgb(image_rgb)
+    # plt.imshow(image, cmap=plt.cm.gray)
+    # plt.show()
+    # image_r, image_g, image_b = divide_image_rpg(image_rgb)
+    # image_fusion = fusion_image_rgb(image_r, image_g, image_b)
+    # res = resize(image, (100, 100))
+    # print(image)
 
-    image_r, image_g, image_b = divide_image_rpg(image_rgb)
-    image_fusion = fusion_image_rgb(image_r, image_g, image_b)
-
-    print(image_rgb.shape)
-
-    res = resize(image_rgb, (100, 100))
-    print(res.shape)
-
-    plt.imshow(res)
+    # image_gray = get_image_gray(image)
+    image_gray = resize(image, (150, 150))
+    res = equalization(image_gray)
+    res = image_gray
+    res = convolution_sobel(res)
+    print(res)
+    # res = otsu(res)
+    # res = equalization(res)
+    # plt.imshow(res, cmap=plt.cm.gray, vmin=0, vmax=255)
+    plt.imshow(res, cmap=plt.cm.gray)
     plt.show()
 
 
 main()
+
+
+def test():
+    img = cv2.imread("test_images/1_2.png")  # Read image
+
+    t_lower = 100  # Lower Threshold
+    t_upper = 200  # Upper threshold
+    aperture_size = 5  # Aperture size
+    L2Gradient = True  # Boolean
+
+    # Applying the Canny Edge filter with L2Gradient = True
+    edge = cv2.Canny(img, t_lower, t_upper, L2gradient=L2Gradient)
+
+    cv2.imshow('original', img)
+    cv2.imshow('edge', edge)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# test()
