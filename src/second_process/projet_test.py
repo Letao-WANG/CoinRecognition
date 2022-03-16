34150e8349069489a3b0105d8a65257d216b0ea0
Letao WANG <letaowang7@gmail.com>
2022-03-16 23:36:12 +0100

    Syu

--- src/second_process/projet_test.py
+++ src/second_process/projet_test.py
@@ -1,12 +1,8 @@
 import math
 import cv2
-
-from util_func.util import *
-
-
-
-# plt.imshow(img, cmap=plt.cm.gray, vmin=0, vmax=max_val)
-# plt.show()
+from src.util_func.util import *
+from PIL import Image
+from numpy import average, dot, linalg
 
 
 def filter_sobel_x():
@@ -62,7 +58,7 @@ def convolution_filter(gray_img, kernel):
 
 
 def main():
-    image = (mpimg.imread('test_images/1_1.png').copy() * 255).astype(np.uint8)
+    image = (mpimg.imread('test_images/1_2.png').copy() * 255).astype(np.uint8)
     # image = mpimg.imread('test_images/Bikesgray.jpg')
     # image = (mpimg.imread('test_images/1_2.png').copy() * 255).astype(np.uint8)
     image = get_image_rgb(image)
@@ -76,11 +72,11 @@ def main():
     # print(image)
 
     # image_gray = get_image_gray(image)
-    image_gray = resize(image, (150, 150))
+    image_gray = resize(image, (300, 300))
     res = equalization(image_gray)
     res = image_gray
-    res = convolution_sobel(res)
-    print(res)
+    # res = convolution_sobel(res)
+    # print(res)
     # res = otsu(res)
     # res = equalization(res)
     # plt.imshow(res, cmap=plt.cm.gray, vmin=0, vmax=255)
@@ -88,7 +84,7 @@ def main():
     plt.show()
 
 
-main()
+# main()
 
 
 def test():
@@ -108,3 +104,41 @@ def test():
     cv2.destroyAllWindows()
 
 # test()
+
+
+def get_thum(image, size=(300, 300), greyscale=False):
+    # 利用image对图像大小重新设置, Image.ANTIALIAS为高质量的
+    image = image.resize(size, Image.ANTIALIAS)
+    if greyscale:
+        # 将图片转换为L模式，其为灰度图，其每个像素用8个bit表示
+        image = image.convert('L')
+    return image
+# 计算图片的余弦距离
+def image_similarity_vectors_via_numpy(image1, image2):
+    image1 = get_thum(image1)
+    image2 = get_thum(image2)
+    images = [image1, image2]
+    vectors = []
+    norms = []
+    for image in images:
+        vector = []
+        for pixel_tuple in image.getdata():
+            vector.append(average(pixel_tuple))
+        vectors.append(vector)
+        # linalg=linear（线性）+algebra（代数），norm则表示范数
+        # 求图片的范数
+        norms.append(linalg.norm(vector, 2))
+    a, b = vectors
+    a_norm, b_norm = norms
+    # dot返回的是点积，对二维数组（矩阵）进行计算
+    res = dot(a / a_norm, b / b_norm)
+    return res
+
+def test2():
+    image1 = Image.open('test_images/Bikesgray.jpg')
+    # image1 = Image.open('test_images/1_1.png')
+    image2 = Image.open('test_images/1_1.png')
+    cosin = image_similarity_vectors_via_numpy(image1, image2)
+    print('Image cosine similarity', cosin)
+
+test2()
